edf11cbc7c61eb28c0887f4a7861bd0a295b6147
Albert1302 <albertyucracalderon@gmail.com>
2024-09-03 00:48:10 -0400

    interfaz del menu producto y cliente

---
+++ Mi_Proyecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mi_proyecto;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Mi_Proyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
